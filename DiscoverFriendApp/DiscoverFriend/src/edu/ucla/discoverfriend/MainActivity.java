cfbaf370385a719936eefe014c3f807869568da4
Hongyi Wang <whynpc@gmail.com>
2014-02-13 23:13:16 -0800

    Hello world for facebook

--- DiscoverFriendApp/DiscoverFriend/src/edu/ucla/discoverfriend/MainActivity.java
+++ DiscoverFriendApp/DiscoverFriend/src/edu/ucla/discoverfriend/MainActivity.java
@@ -1,21 +1,65 @@
 package edu.ucla.discoverfriend;
 
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	private TextView textView1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		textView1 = (TextView) findViewById(R.id.textView1);
+		
+		Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+		      // callback when session changes state
+		      @Override
+		      public void call(Session session, SessionState state, Exception exception) {
+		        if (session.isOpened()) {
+
+		          // make request to the /me API
+		          Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+
+		            // callback after Graph API response with user object
+		            @Override
+		            public void onCompleted(GraphUser user, Response response) {
+		              if (user != null) {
+		            	  textView1.setText(user.getName());
+		              }
+		            }
+		          });
+		        }
+		      }
+
+			
+		    });
 	}
+	
+	@Override
+	  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	      super.onActivityResult(requestCode, resultCode, data);
+	      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	  }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		
 		return true;
 	}
 
