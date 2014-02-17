1d77094e07f3070efae3cc892c1af5b677b8f6f1
Eric Chung <chung.yanhon@gmail.com>
2014-02-16 18:39:46 -0800

    added login flow and fql query

--- DiscoverFriendApp/DiscoverFriend/src/edu/ucla/discoverfriend/MainActivity.java
+++ DiscoverFriendApp/DiscoverFriend/src/edu/ucla/discoverfriend/MainActivity.java
@@ -1,65 +1,102 @@
 package edu.ucla.discoverfriend;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TextView;
+
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.model.GraphUser;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
-import android.widget.TextView;
+public class MainActivity extends FragmentActivity {
 
-public class MainActivity extends Activity {
-	
 	private TextView textView1;
+	private MainFragment mainFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		if (savedInstanceState == null) {
+			// Add the fragment on initial activity setup
+			mainFragment = new MainFragment();
+			getSupportFragmentManager().beginTransaction()
+			.add(android.R.id.content, mainFragment)
+			.commit();
+		} else {
+			// Or set the fragment from restored state info
+			mainFragment = (MainFragment) getSupportFragmentManager()
+			.findFragmentById(android.R.id.content);
+		}
+
+		try {
+			PackageInfo info = getPackageManager().getPackageInfo(
+					"com.facebook.samples.loginhowto", 
+					PackageManager.GET_SIGNATURES);
+			for (Signature signature : info.signatures) {
+				MessageDigest md = MessageDigest.getInstance("SHA");
+				md.update(signature.toByteArray());
+				Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+			}
+		} catch (NameNotFoundException e) {
+
+		} catch (NoSuchAlgorithmException e) {
+
+		}
 		setContentView(R.layout.activity_main);
-		
-		
+
+
 		textView1 = (TextView) findViewById(R.id.textView1);
-		
+
 		Session.openActiveSession(this, true, new Session.StatusCallback() {
 
-		      // callback when session changes state
-		      @Override
-		      public void call(Session session, SessionState state, Exception exception) {
-		        if (session.isOpened()) {
+			// callback when session changes state
+			@Override
+			public void call(Session session, SessionState state, Exception exception) {
+				if (session.isOpened()) {
 
-		          // make request to the /me API
-		          Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+					// make request to the /me API
+					Request.newMeRequest(session, new Request.GraphUserCallback() {
 
-		            // callback after Graph API response with user object
-		            @Override
-		            public void onCompleted(GraphUser user, Response response) {
-		              if (user != null) {
-		            	  textView1.setText(user.getName());
-		              }
-		            }
-		          });
-		        }
-		      }
+						// callback after Graph API response with user object
+						@Override
+						public void onCompleted(GraphUser user, Response response) {
+							if (user != null) {
+								textView1.setText(user.getName());
+							}
+						}
+					}).executeAsync();
+				}
+			}
 
-			
-		    });
+
+		});
 	}
-	
+
 	@Override
-	  public void onActivityResult(int requestCode, int resultCode, Intent data) {
-	      super.onActivityResult(requestCode, resultCode, data);
-	      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
-	  }
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		
+
 		return true;
 	}
 
---
+++ DiscoverFriendApp/DiscoverFriend/src/edu/ucla/discoverfriend/MainFragment.java
@@ -0,0 +1,133 @@
+package edu.ucla.discoverfriend;
+
+import java.util.Arrays;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.LoginButton;
+
+public class MainFragment extends Fragment {
+
+	private static final String TAG = "MainFragment";
+	private UiLifecycleHelper uiHelper;
+
+	private Button queryButton;
+	private Button multiQueryButton;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, 
+			ViewGroup container, 
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.activity_main, container, false);
+
+		// To allow the fragment to receive the onActivityResult()
+		LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
+		authButton.setFragment(this);
+		
+		queryButton = (Button) view.findViewById(R.id.queryButton);
+		multiQueryButton = (Button) view.findViewById(R.id.multiQueryButton);
+
+		queryButton.setOnClickListener(new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+		        String fqlQuery = "SELECT uid, name, pic_square FROM user WHERE uid IN " +
+		              "(SELECT uid2 FROM friend WHERE uid1 = me() LIMIT 25)";
+		        Bundle params = new Bundle();
+		        params.putString("q", fqlQuery);
+		        Session session = Session.getActiveSession();
+		        Request request = new Request(session,
+		            "/fql",                         
+		            params,                         
+		            HttpMethod.GET,                 
+		            new Request.Callback(){         
+		                public void onCompleted(Response response) {
+		                    Log.i(TAG, "Result: " + response.toString());
+		                }                  
+		        }); 
+		        Request.executeBatchAsync(request);                 
+		    }
+		});
+
+		return view;
+	}
+
+	private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+		if (state.isOpened()) {
+			Log.i(TAG, "Logged in...");
+			// Display authenticated UI such as acquire Facebook friend list 
+			queryButton.setVisibility(View.VISIBLE);
+			multiQueryButton.setVisibility(View.VISIBLE);
+		} else if (state.isClosed()) {
+			Log.i(TAG, "Logged out...");
+			queryButton.setVisibility(View.INVISIBLE);
+			multiQueryButton.setVisibility(View.INVISIBLE);
+		}
+	}
+
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+		@Override
+		public void call(Session session, SessionState state, Exception exception) {
+			onSessionStateChange(session, state, exception);
+		}
+	};
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		uiHelper = new UiLifecycleHelper(getActivity(), callback);
+		uiHelper.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+		// For scenarios where the main activity is launched and user
+		// session is not null, the session state change notification
+		// may not be triggered. Trigger it if it's open/closed.
+		Session session = Session.getActiveSession();
+		if (session != null &&
+				(session.isOpened() || session.isClosed()) ) {
+			onSessionStateChange(session, session.getState(), null);
+		}
+		uiHelper.onResume();
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		uiHelper.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		uiHelper.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		uiHelper.onDestroy();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		uiHelper.onSaveInstanceState(outState);
+	}
+
+}
